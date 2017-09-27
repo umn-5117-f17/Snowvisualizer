932706329fa7430cf7eb1343b5c73d558fdf6fb4
Qing Hong <hongqingnothing@163.com>
2017-09-27 12:09:05 -0500

    update map visualizaiont v1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/map', function(req, res, next) {
+  res.render('googlemap', { title: 'Express' });
+});
+
 module.exports = router;
